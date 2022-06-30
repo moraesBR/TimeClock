fae1d1e3ed52ca9c60378ef66cb3eb83afa554ef
moraesbr <albert.richard@gmail.com>
2022-06-29 23:54:20 -0300

    Request Location

--- app/src/main/java/com/materdei/timeclock/MainActivity.kt
+++ app/src/main/java/com/materdei/timeclock/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.materdei.timeclock
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+//import android.location.LocationRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var PERMISSION_ID = 1000
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var location: Location
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,5 +38,101 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         },3000)*/
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getLastLocation()
+    }
+
+    private fun getLastLocation(){
+        // check permissions
+        if(checkPermission()){
+            // check location service
+            if(isLocationEnabled()){
+                // let's get the location
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener{ task ->
+                    location = task.result
+                    if (location == null){
+                        getNewLocation()
+                    }
+                    Toast.makeText(this, "Lat ${location.altitude}; Lon ${location.longitude}", Toast.LENGTH_LONG).show()
+                }
+            }
+            else{
+                Toast.makeText(this,"Please Enable your Location service",Toast.LENGTH_LONG).show()
+            }
+        }
+        else{
+            requestPermission()
+        }
+    }
+
+    private fun getNewLocation(){
+        locationRequest = com.google.android.gms.location.LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 2
+        if(checkPermission()) {
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest, locationCallback, Looper.myLooper()
+            )
+        }
+    }
+
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(p0: LocationResult) {
+            location = p0.lastLocation!!
+        }
+    }
+
+    /* Checking if the user has granted location precisely permission   */
+    private fun checkPermission(): Boolean{
+        if(ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        /*&& ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            == PackageManager.PERMISSION_GRANTED*/
+        && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+                )
+            return true
+        else
+            return false
+    }
+
+    /* Request location permissions to user */
+    private fun requestPermission(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION),PERMISSION_ID
+        )
+    }
+
+    /* Checking if Location service of the device is enabled */
+    private fun isLocationEnabled(): Boolean{
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSION_ID){
+            if(grantResults.isNotEmpty()
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                Log.d("Debug","You have the permission")
+            }
+        }
+
     }
 }
